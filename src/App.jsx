06d3324c4cbfd85159599700912ce2d434bc67bc
nikilr7 <nikilraja11@gmail.com>
2025-07-20 15:05:47 +0530

    SRC

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import LandingPage from './LandingPage'
 
 const App = () => {
   return (
-    <div>App</div>
+    <React.Fragment>
+      <ToastContainer/>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<LandingPage/>}/>
+   </Routes>
+   </BrowserRouter>
+   </React.Fragment>
   )
 }
 
---
+++ src/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <React.Fragment>
+        <div>
+        <h1 className='bg-amber-700 text-5xl  text-blue-50 text-center h-full pb-10'>Landing Page</h1>
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default LandingPage
